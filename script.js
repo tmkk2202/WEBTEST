ef7ecc2d642ce349bf4d4ce87ce7d1594d2110cf
TM <22thetmon@gmail.com>
2023-10-01 11:57:15 +0630

    ?

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 console.log("hello guys!");
-let name = "a";
-console.log(name);
-var age = 2;
-console.log(age);
-name = "b";
-console.log(name);
-age = 3;
-console.log(age);
-var age = 66;
-console.log(age);
-name = "cc";
-console.log(name);
 
 if (true) {
     var age =22;
