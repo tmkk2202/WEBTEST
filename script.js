8e56648b6c1d7cdadf89198ae8a49df827a82172
TM <22thetmon@gmail.com>
2023-10-01 10:54:13 +0630

    js var let testing

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("hello guys!");
+let name = "a";
+console.log(name);
+var age = 2;
+console.log(age);
+name = "b";
+console.log(name);
+age = 3;
+console.log(age);
+var age = 66;
+console.log(age);
+name = "cc";
+console.log(name);
 
